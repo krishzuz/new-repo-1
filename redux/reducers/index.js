5f8734c18541b4bf90cf5665b007c8fc279eab27
krish <72214033+krishzuz@users.noreply.github.com>
2021-07-07 13:47:57 +0530

    Delete redux directory

--- redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { newReducer, newReducer1 } from "./newReducer";
-
-const allReducers = combineReducers({
-  allData: newReducer,
-  addTask: newReducer1,
-});
-
-export default allReducers;
